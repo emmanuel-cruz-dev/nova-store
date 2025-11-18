85b87d3cb625dbf7b7751681ca96563c0b55ed29
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-18 12:37:37 -0300

    refactor: removes comments in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
 import "bootswatch/dist/cerulean/bootstrap.css";
-//import "bootswatch/dist/materia/bootstrap.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
