c169855428211b3b4762bd8ecf53568a5a8c5496
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-20 07:48:43 -0300

    refactor: updates imports in src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context";
 import App from "./App.jsx";
 import "bootswatch/dist/cerulean/bootstrap.css";
 import "./index.css";
