cb7bf5b44859ce6661c7973a98b8e003e9b73b06
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-21 07:08:31 -0300

    feat: adds index.js to src/hooks/

---
+++ src/hooks/index.js
@@ -0,0 +1,20 @@
+export { useUpdateUser } from "./auth/useUpdateUser";
+export { useAuth, AuthContext } from "./useAuth";
+export { useCart, CartContext } from "./useCart";
+export { useCategorySelect } from "./useCategorySelect";
+export { useCheckoutOrder } from "./useCheckoutOrder";
+export { useNavigation } from "./useNavigation";
+export { useNavigationBar } from "./useNavigationBar";
+export { useNewsletterSubscription } from "./useNewsletterSubscription";
+export { usePagination } from "./usePagination";
+export { useProductCard } from "./useProductCard";
+export {
+  useProducts,
+  useProductById,
+  useActiveProducts,
+  useProductsByCategory,
+  useDeleteProduct,
+} from "./useProducts";
+export { useQueryHandler } from "./useQueryHandler";
+export { useScroll } from "./useScroll";
+export { useUsers, useUserById, useUserByRole } from "./useUsers";
