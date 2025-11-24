dc46ae1e7a033970d59ac17001c2e51e4d220858
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-24 17:00:39 -0300

    refactor: updates images imports in src/data/heroBannerImgs.js

--- src/data/heroBannerImgs.js
+++ src/data/heroBannerImgs.js
@@ -1,8 +1,8 @@
-import HeroBanner1 from "../assets/hero-slider-1.webp";
-import HeroBanner2 from "../assets/hero-slider-2.webp";
-import HeroBanner3 from "../assets/hero-slider-3.webp";
-import HeroBanner4 from "../assets/hero-slider-4.webp";
-import HeroBanner5 from "../assets/hero-slider-5.webp";
+import HeroBanner1 from "../assets/hero-slider1.webp";
+import HeroBanner2 from "../assets/hero-slider2.webp";
+import HeroBanner3 from "../assets/hero-slider3.webp";
+import HeroBanner4 from "../assets/hero-slider4.webp";
+import HeroBanner5 from "../assets/hero-slider5.webp";
 
 export const heroBannerImgs = [
   HeroBanner1,
