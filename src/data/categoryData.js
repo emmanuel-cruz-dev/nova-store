72f4299b1643adb4f9069a7c225b384d87f96837
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-26 11:06:39 -0300

    feat: adds categoryData.js to src/data/

---
+++ src/data/categoryData.js
@@ -0,0 +1,22 @@
+export const categoryData = {
+  tecnologia: {
+    id: "tecnologia",
+    name: "Tecnología",
+    image: "/src/assets/tecnologia.webp",
+  },
+  gaming: {
+    id: "gaming",
+    name: "Gaming",
+    image: "/src/assets/gaming.webp",
+  },
+  hombres: {
+    id: "hombres",
+    name: "Ropa de Hombre",
+    image: "/src/assets/indumentaria-masculina.webp",
+  },
+  mujeres: {
+    id: "mujeres",
+    name: "Ropa de Mujer",
+    image: "/src/assets/indumentaria-femenina.webp",
+  },
+};
