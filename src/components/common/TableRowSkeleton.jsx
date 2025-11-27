3a4abce7ac53f948ba5414d4c6258d85df53261e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-27 09:15:31 -0300

    feat: adds TableRowSkeleton.jsx to src/components/common/

---
+++ src/components/common/TableRowSkeleton.jsx
@@ -0,0 +1,23 @@
+function TableRowSkeleton() {
+  return (
+    <tr>
+      <td>
+        <span className="placeholder col-12 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-4 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-3 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-4 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-8 placeholder-wave rounded"></span>
+      </td>
+    </tr>
+  );
+}
+
+export default TableRowSkeleton;
