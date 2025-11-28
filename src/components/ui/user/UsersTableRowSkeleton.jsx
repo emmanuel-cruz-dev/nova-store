1cefeaeccc9c14f6e7f51c119bb76368ad26d84f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-28 08:03:34 -0300

    feat: adds UsersTableRowSkeleton.jsx to src/components/ui/user/

---
+++ src/components/ui/user/UsersTableRowSkeleton.jsx
@@ -0,0 +1,20 @@
+function UsersTableRowSkeleton() {
+  return (
+    <tr>
+      <td>
+        <span className="placeholder col-4 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-6 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-8 placeholder-wave rounded"></span>
+      </td>
+      <td>
+        <span className="placeholder col-12 placeholder-wave rounded"></span>
+      </td>
+    </tr>
+  );
+}
+
+export default UsersTableRowSkeleton;
