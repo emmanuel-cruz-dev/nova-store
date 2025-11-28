bf04b59c97fa5c74f56f93f5aeeb45b22d91dcbf
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-28 08:06:42 -0300

    feat: adds formatDateShort and formatDateDetailed functions to src/utils/utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -93,3 +93,24 @@ export const getColSize = (displayCategories) => {
   if (count === 3) return 4;
   return 3;
 };
+
+export function formatDateShort(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("es-ES", {
+    day: "2-digit",
+    month: "numeric",
+    year: "2-digit",
+  });
+}
+
+export function formatDateDetailed(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("es-ES", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+}
