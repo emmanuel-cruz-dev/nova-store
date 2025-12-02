1f30b25ced7b8458113d1e155d8db7f203aedc9c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-02 11:55:06 -0300

    feat: adds types to src/hooks/useCart.ts

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,8 +1,11 @@
 import { createContext, useContext } from "react";
+import { CartContextType } from "../types";
 
-export const CartContext = createContext(undefined);
+export const CartContext = createContext<CartContextType | undefined>(
+  undefined
+);
 
-export const useCart = () => {
+export const useCart = (): CartContextType => {
   const context = useContext(CartContext);
   if (!context) {
     throw new Error("useCart must be used within a CartProvider");
