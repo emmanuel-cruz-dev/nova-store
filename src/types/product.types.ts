efab03af8246bb6afc9c0f97db88978a8ff68b41
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-02 10:40:04 -0300

    feat: adds common.types, product.types, user.types and index.ts to src/types/

---
+++ src/types/product.types.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  name: string;
+  brand: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  stock: number;
+  rating: number;
+  isActive: boolean;
+}
