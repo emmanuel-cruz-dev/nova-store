f8141561cd0e0d8d6ad256567c0490eca3ab0ed8
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-04 10:11:36 -0300

    feat: adds types to src/hooks/useProductCard.ts

--- src/hooks/useProductCard.ts
+++ src/hooks/useProductCard.ts
@@ -1,19 +1,22 @@
 import { useCart } from "./useCart";
+import { ProductToAdd } from "../types";
 
 export const useProductCard = ({
   id,
   name,
+  brand,
   price,
   category,
   description,
   image,
-}) => {
+}: ProductToAdd) => {
   const { handleAddToCart } = useCart();
 
   const handleAddToCartClick = (quantity = 1) => {
     const product = {
       id,
       name,
+      brand,
       price,
       category,
       description,
