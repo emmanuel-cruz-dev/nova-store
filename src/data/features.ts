df4f54bed9e4e8fdb6e4088c90a7b4dc3274cb3e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-05 10:37:38 -0300

    docs: renames features.tsx to features.ts and highlightedFeatures.tsx to highlightedFeatures.ts in src/data/

--- src/data/features.ts
+++ src/data/features.ts
@@ -3,31 +3,31 @@ import { DeliveryTruck, Undo, Wallet, Gift, HeadsetHelp } from "../icons";
 export const features = [
   {
     id: 1,
-    icon: <DeliveryTruck />,
+    icon: DeliveryTruck,
     title: "Envío gratuito",
     subtitle: "Para todos los pedidos superiores a $1000",
   },
   {
     id: 2,
-    icon: <Undo />,
+    icon: Undo,
     title: "30 días de devolución",
     subtitle: "Para cambio de producto",
   },
   {
     id: 3,
-    icon: <Wallet />,
+    icon: Wallet,
     title: "Pago seguro",
     subtitle: "Se aceptan tarjetas de crédito",
   },
   {
     id: 4,
-    icon: <Gift />,
+    icon: Gift,
     title: "Regalos especiales",
     subtitle: "En el primer pedido de producto",
   },
   {
     id: 5,
-    icon: <HeadsetHelp />,
+    icon: HeadsetHelp,
     title: "Soporte 24/7",
     subtitle: "Contáctanos cuando quieras",
   },
