c1a642cae77fa551b26c36bf51bd4598ec544e07
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-06 15:34:26 -0300

    feat adds types to src/hooks/auth/useUpdateUser.ts

--- src/hooks/auth/useUpdateUser.ts
+++ src/hooks/auth/useUpdateUser.ts
@@ -1,22 +1,29 @@
 import { mutate } from "swr";
 import useSWRMutation from "swr/mutation";
 import { userAuthService } from "../../api/services/auth/user.service";
+import { User, UseUpdateUserReturn } from "../../types";
 
-export const useUpdateUser = () => {
-  const { trigger, isMutating, error } = useSWRMutation(
+export const useUpdateUser = (): UseUpdateUserReturn => {
+  const { trigger, isMutating, error } = useSWRMutation<
+    User,
+    Error,
+    string,
+    User
+  >(
     "updateUser",
-    async (_, { arg: userData }) => {
+    async (_, { arg: userData }: { arg: User }) => {
       return await userAuthService.updateUser(userData);
     },
     {
-      onSuccess: (updatedUser) => {
-        if (updatedUser?.id || updatedUser?._id) {
-          const userId = updatedUser.id || updatedUser._id;
-          mutate(["user", userId]);
+      onSuccess: (updatedUser: User) => {
+        if (updatedUser?.id) {
+          mutate(["user", updatedUser.id]);
         }
 
         mutate("users");
-        mutate((key) => Array.isArray(key) && key[0] === "usersByRole");
+        mutate(
+          (key: unknown) => Array.isArray(key) && key[0] === "usersByRole"
+        );
       },
     }
   );
