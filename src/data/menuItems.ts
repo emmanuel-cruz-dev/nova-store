ee7a227659166b21991c14445ab6062377563807
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-08 16:48:24 -0300

    feat: adds types to src/data/menuItems.ts

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -1,13 +1,14 @@
 import { User, LogOut, Boxes, Users } from "lucide-react";
+import { MenuItem } from "../types";
 
-export const adminMenuItems = [
+export const adminMenuItems: MenuItem[] = [
   { id: "profile", icon: User, label: "Mi Perfil" },
   { id: "products", icon: Boxes, label: "Todos los Productos" },
   { id: "users", icon: Users, label: "Todos los Usuarios" },
   { id: "logout", icon: LogOut, label: "Cerrar Sesión" },
 ];
 
-export const userMenuItems = [
+export const userMenuItems: MenuItem[] = [
   { id: "profile", icon: User, label: "Mi Perfil" },
   { id: "logout", icon: LogOut, label: "Cerrar Sesión" },
 ];
