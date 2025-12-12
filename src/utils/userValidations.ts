32e09cf3e3dffbae6334d026038eeffc77b503fd
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-12 10:15:46 -0300

    feat: adds userValidations.ts to src/utils/

---
+++ src/utils/userValidations.ts
@@ -0,0 +1,63 @@
+import { UserData } from "../types";
+import { isValidUrl } from "./productValidations";
+
+export const validateLoginForm = (loginData: {
+  email: string;
+  password: string;
+}) => {
+  const errors = {} as {
+    [key: string]: string;
+  };
+
+  if (!loginData.email.trim()) {
+    errors.email = "El email es requerido";
+  } else if (!/\S+@\S+\.\S+/.test(loginData.email)) {
+    errors.email = "El email no es válido";
+  }
+
+  if (!loginData.password) {
+    errors.password = "La contraseña es requerida";
+  }
+
+  return errors;
+};
+
+export const validateRegisterForm = (
+  formData: UserData & { confirmPassword: string }
+) => {
+  const errors = {} as {
+    [key: string]: string;
+  };
+
+  if (!formData.firstName.trim()) {
+    errors.firstName = "El nombre es requerido";
+  }
+
+  if (!formData.lastName.trim()) {
+    errors.lastName = "El apellido es requerido";
+  }
+
+  if (!formData.email.trim()) {
+    errors.email = "El email es requerido";
+  } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+    errors.email = "El email no es válido";
+  }
+
+  if (!formData.password) {
+    errors.password = "La contraseña es requerida";
+  } else if (formData.password.length < 6) {
+    errors.password = "La contraseña debe tener al menos 6 caracteres";
+  }
+
+  if (!formData.confirmPassword) {
+    errors.confirmPassword = "Debe confirmar la contraseña";
+  } else if (formData.password !== formData.confirmPassword) {
+    errors.confirmPassword = "Las contraseñas no coinciden";
+  }
+
+  if (formData.avatar && !isValidUrl(formData.avatar)) {
+    errors.avatar = "La URL del avatar no es válida";
+  }
+
+  return errors;
+};
