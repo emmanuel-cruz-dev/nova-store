89200ee061e8463fdd1518cb750ab849ea262ca2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-12 12:21:04 -0300

    refactor: removes commented functions in src/utils/utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -28,40 +28,6 @@ export function renderCategory(category: string) {
       return "Todos los productos";
   }
 }
-//   const errors = {};
-
-//   if (!formData.firstName.trim()) {
-//     errors.firstName = "El nombre es requerido";
-//   }
-
-//   if (!formData.lastName.trim()) {
-//     errors.lastName = "El apellido es requerido";
-//   }
-
-//   if (!formData.email.trim()) {
-//     errors.email = "El email es requerido";
-//   } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-//     errors.email = "El email no es válido";
-//   }
-
-//   if (!formData.password) {
-//     errors.password = "La contraseña es requerida";
-//   } else if (formData.password.length < 6) {
-//     errors.password = "La contraseña debe tener al menos 6 caracteres";
-//   }
-
-//   if (!formData.confirmPassword) {
-//     errors.confirmPassword = "Debe confirmar la contraseña";
-//   } else if (formData.password !== formData.confirmPassword) {
-//     errors.confirmPassword = "Las contraseñas no coinciden";
-//   }
-
-//   if (formData.avatar && !isValidUrl(formData.avatar)) {
-//     errors.avatar = "La URL del avatar no es válida";
-//   }
-
-//   return errors;
-// };
 
 export const getColSize = (displayCategories: string[]) => {
   const count = displayCategories.length;
