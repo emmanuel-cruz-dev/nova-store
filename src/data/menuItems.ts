5c33df0e0643a178043136dfefb054235de3efd2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-22 11:05:35 -0300

    feat: adds orders and all-orders to menu items in src/data/menuItems.ts

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -1,8 +1,16 @@
-import { User, LogOut, Boxes, Users } from "lucide-react";
+import {
+  User,
+  LogOut,
+  Boxes,
+  Users,
+  ShoppingCart,
+  ClipboardList,
+} from "lucide-react";
 import { MenuItem } from "../types";
 
 export const adminMenuItems: MenuItem[] = [
   { id: "profile", icon: User, label: "Mi Perfil" },
+  { id: "all-orders", icon: ClipboardList, label: "Todas las Órdenes" },
   { id: "products", icon: Boxes, label: "Todos los Productos" },
   { id: "users", icon: Users, label: "Todos los Usuarios" },
   { id: "logout", icon: LogOut, label: "Cerrar Sesión" },
@@ -10,5 +18,6 @@ export const adminMenuItems: MenuItem[] = [
 
 export const userMenuItems: MenuItem[] = [
   { id: "profile", icon: User, label: "Mi Perfil" },
+  { id: "orders", icon: ShoppingCart, label: "Mis Pedidos" },
   { id: "logout", icon: LogOut, label: "Cerrar Sesión" },
 ];
