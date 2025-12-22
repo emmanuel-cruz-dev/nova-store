32b625f1b4439beb83210c165961926859e7120e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-22 11:11:10 -0300

    feat: adds statusConfig.ts to src/data/

---
+++ src/data/statusConfig.ts
@@ -0,0 +1,6 @@
+export const statusConfig = {
+  pending: { variant: "warning", text: "Pendiente" },
+  processing: { variant: "info", text: "Procesando" },
+  completed: { variant: "success", text: "Completada" },
+  cancelled: { variant: "danger", text: "Cancelada" },
+};
