603dd44a9b8844502c173b43a967dc5b3b38af2e
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-23 11:10:33 -0300

    feat: adds and implements useCheckoutStore in src/components/modals/CheckoutModal.tsx

--- src/components/modals/CheckoutModal.tsx
+++ src/components/modals/CheckoutModal.tsx
@@ -1,25 +1,22 @@
+import { Link } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
 import { CheckCircle, X } from "lucide-react";
 import { useAuthStore } from "../../stores/authStore";
+import { useCheckoutStore } from "../../stores/checkoutStore";
 import { CheckoutSteps, CheckoutSummary } from "..";
-import { CheckoutModalProps } from "../../types";
 
-function CheckoutModal({
-  show,
-  onHide,
-  orderTotal,
-  itemsCount,
-}: CheckoutModalProps) {
+function CheckoutModal() {
   const { user } = useAuthStore();
+  const { showModal, handleCloseModal } = useCheckoutStore();
 
   return (
-    <Modal show={show} onHide={onHide} centered>
+    <Modal show={showModal} onHide={handleCloseModal} centered>
       <Modal.Body
         className="text-center position-relative"
         style={{ paddingBlock: "1.5rem" }}
       >
         <button
-          onClick={onHide}
+          onClick={handleCloseModal}
           className="btn btn-light position-absolute top-0 end-0 m-3 rounded-circle border-0 d-flex align-items-center justify-content-center"
           style={{ width: "2.5rem", height: "2.5rem", padding: 0 }}
           aria-label="Cerrar"
@@ -33,25 +30,28 @@ function CheckoutModal({
         <h2 className="mb-2" style={{ fontSize: "1.75rem" }}>
           ¡Compra realizada con éxito!
         </h2>
-
-        <p className="text-muted mb-3">
+        <p className="text-muted mb-2">
           {user && (
             <>
-              Gracias por tu compra{" "}
+              ¡Gracias por tu compra,{" "}
               <span style={{ textTransform: "capitalize" }}>
                 {user.firstName}
               </span>
-              .{" "}
+              !{" "}
             </>
           )}
-          Tu pedido ha sido procesado correctamente.
+          Todo salió bien. Podés seguir el estado desde{" "}
+          <Link to="/profile/orders" className="text-decoration-none">
+            tus órdenes
+          </Link>
+          .
         </p>
 
-        <CheckoutSummary orderTotal={orderTotal} itemsCount={itemsCount} />
+        <CheckoutSummary />
         <CheckoutSteps />
         <Button
           variant="primary"
-          onClick={onHide}
+          onClick={handleCloseModal}
           style={{ padding: "0.5rem 3rem" }}
         >
           Entendido
