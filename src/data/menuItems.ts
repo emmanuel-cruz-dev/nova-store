7aaa6054db70be0f506f7f30e956e6b30f3ff132
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-16 08:42:42 -0300

    feat: adds dashboard and updates text content in src/data/menuItems.ts

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -1,17 +1,26 @@
-import { User, LogOut, Boxes, Users, ClipboardList, Heart } from "lucide-react";
+import {
+  User,
+  LogOut,
+  Boxes,
+  Users,
+  ClipboardList,
+  Heart,
+  LayoutDashboard,
+} from "lucide-react";
 import { MenuItem } from "../types";
 
 export const adminMenuItems: MenuItem[] = [
-  { id: "profile", icon: User, label: "Mi Perfil" },
-  { id: "all-orders", icon: ClipboardList, label: "Todas las Órdenes" },
-  { id: "products", icon: Boxes, label: "Todos los Productos" },
-  { id: "users", icon: Users, label: "Todos los Usuarios" },
-  { id: "logout", icon: LogOut, label: "Cerrar Sesión" },
+  { id: "dashboard", icon: LayoutDashboard, label: "Panel de control" },
+  { id: "profile", icon: User, label: "Mi perfil" },
+  { id: "all-orders", icon: ClipboardList, label: "Órdenes" },
+  { id: "products", icon: Boxes, label: "Productos" },
+  { id: "users", icon: Users, label: "Usuarios" },
+  { id: "logout", icon: LogOut, label: "Cerrar sesión" },
 ];
 
 export const userMenuItems: MenuItem[] = [
-  { id: "profile", icon: User, label: "Mi Perfil" },
-  { id: "orders", icon: ClipboardList, label: "Mis Órdenes" },
+  { id: "profile", icon: User, label: "Mi perfil" },
+  { id: "orders", icon: ClipboardList, label: "Mis órdenes" },
   { id: "favorites", icon: Heart, label: "Favoritos" },
-  { id: "logout", icon: LogOut, label: "Cerrar Sesión" },
+  { id: "logout", icon: LogOut, label: "Cerrar sesión" },
 ];
