887c7f8b3d781ab923d1cd9d830c36053e3217c1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-16 11:22:11 -0300

    feat: adds SectionHeader to src/components/ui/user/AdminOrdersTable.tsx

--- src/components/ui/user/AdminOrdersTable.tsx
+++ src/components/ui/user/AdminOrdersTable.tsx
@@ -1,6 +1,6 @@
 import { Spinner, Badge, Card, Form, Accordion } from "react-bootstrap";
 import { Bounce, ToastContainer } from "react-toastify";
-import { Calendar, User, Mail } from "lucide-react";
+import { Calendar, User, Mail, Clipboard } from "lucide-react";
 import { useAdminOrders } from "../../../hooks/useAdminOrders";
 import {
   EmptySection,
@@ -8,6 +8,7 @@ import {
   OrderItemsList,
   OrderTableSummary,
   OrdersLoader,
+  SectionHeader,
 } from "../../../components";
 import { getStatusBadge } from "../../../helpers/GetStatusBadge";
 import { formatDateDetailed, formatPrice } from "../../../utils";
@@ -21,15 +22,6 @@ function AdminOrdersTable() {
 
   if (error) return <ErrorMessage message="Error al cargar las órdenes" />;
 
-  if (orders.length === 0) {
-    return (
-      <EmptySection
-        title="No hay órdenes registradas"
-        message="Las órdenes de los clientes aparecerán aquí."
-      />
-    );
-  }
-
   const handleStatusChange = (
     userId: number,
     orderId: string,
@@ -40,97 +32,127 @@ function AdminOrdersTable() {
 
   return (
     <section>
-      <header className="d-flex justify-content-between align-items-center mb-4">
-        <h2>Gestión de Órdenes</h2>
-        <Badge bg="secondary">{orders.length} órdenes</Badge>
-      </header>
+      <div className="d-flex justify-content-between">
+        <SectionHeader
+          title="Gestión de órdenes"
+          subtitle="Administra las órdenes del sistema"
+        />
 
-      <Accordion alwaysOpen={false} className="d-flex flex-column gap-3">
-        {orders.map((order, index) => (
-          <Accordion.Item
-            eventKey={String(index)}
-            key={order.id}
-            className="shadow-sm border-0"
-            style={{ borderRadius: "0.5rem", overflow: "hidden" }}
-          >
-            <Card className="border-0">
-              <Accordion.Header>
-                <div className="w-100 d-md-flex justify-content-between align-items-start">
-                  <div className="mb-3 mb-md-0">
-                    <div className="d-flex align-items-center gap-2 mb-2">
-                      <strong>Orden #{order.id.slice(0, 6)}</strong>
-                      {getStatusBadge(order.status)}
-                    </div>
+        <span className="mt-3">
+          {orders.length === 0 ? (
+            <Badge bg="secondary">No hay órdenes registradas</Badge>
+          ) : (
+            <Badge bg="secondary">{orders.length} órdenes</Badge>
+          )}
+        </span>
+      </div>
 
-                    <ul className="d-flex flex-wrap gap-3 custom__text-muted small p-0">
-                      <li className="d-flex align-items-center gap-1">
-                        <Calendar size={14} />
-                        {formatDateDetailed(order.createdAt)}
-                      </li>
-                      <li className="d-flex align-items-center gap-1">
-                        <User size={14} />
-                        {order.userName}
-                      </li>
-                      <li className="d-flex align-items-center gap-1">
-                        <Mail size={14} />
-                        {order.userEmail}
-                      </li>
-                    </ul>
-                  </div>
-
-                  <div className="d-flex flex-md-column justify-content-between gap-2 text-end me-md-3">
-                    <div className="fw-bold fs-5 custom__text-primary">
-                      ${formatPrice(order.total)}
-                    </div>
-
-                    {updatingOrderId === order.id ? (
-                      <div className="d-flex align-items-center gap-2 mt-1 custom__text-muted small">
-                        <Spinner animation="border" size="sm" />
-                        Actualizando estado...
+      {orders.length === 0 ? (
+        <EmptySection
+          title="No hay órdenes registradas"
+          message="Las órdenes de los clientes aparecerán aquí."
+          icon={<Clipboard size={56} className="text-white" />}
+        />
+      ) : (
+        <Accordion
+          alwaysOpen={false}
+          className="d-flex flex-column gap-3"
+          style={{ marginTop: "-14px" }}
+        >
+          {orders.map((order, index) => (
+            <Accordion.Item
+              eventKey={String(index)}
+              key={order.id}
+              className="shadow-sm border-0"
+              style={{ borderRadius: "0.5rem", overflow: "hidden" }}
+            >
+              <Card className="border-0">
+                <Accordion.Header>
+                  <div className="w-100 d-md-flex justify-content-between align-items-start">
+                    <div className="mb-3 mb-md-0">
+                      <div className="d-flex align-items-center gap-2 mb-2">
+                        <strong>Orden #{order.id.slice(0, 6)}</strong>
+                        {getStatusBadge(order.status)}
                       </div>
-                    ) : (
-                      <Form.Select
-                        id={`status-${order.id}`}
-                        name={`status-${order.id}`}
-                        size="sm"
-                        value={order.status}
-                        disabled={updatingOrderId === order.id}
-                        onClick={(e) => e.stopPropagation()}
-                        onChange={(e) =>
-                          handleStatusChange(
-                            order.userId,
-                            order.id,
-                            e.target.value as Order["status"]
-                          )
-                        }
-                        style={{ width: "150px" }}
-                      >
-                        <option value="pending">Pendiente</option>
-                        <option value="processing">Procesando</option>
-                        <option value="completed">Completada</option>
-                        <option value="cancelled">Cancelada</option>
-                      </Form.Select>
-                    )}
+
+                      <ul className="d-flex flex-wrap gap-3 custom__text-muted small p-0">
+                        <li className="d-flex align-items-center gap-1">
+                          <Calendar size={14} />
+                          {formatDateDetailed(order.createdAt)}
+                        </li>
+                        <li className="d-flex align-items-center gap-1">
+                          <User size={14} />
+                          {order.userName}
+                        </li>
+                        <li className="d-flex align-items-center gap-1">
+                          <Mail size={14} />
+                          {order.userEmail}
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div className="d-flex flex-md-column justify-content-between gap-2 text-end me-md-3">
+                      <div className="fw-bold fs-5 custom__text-primary">
+                        ${formatPrice(order.total)}
+                      </div>
+
+                      {updatingOrderId === order.id ? (
+                        <div className="d-flex align-items-center gap-2 mt-1 custom__text-muted small">
+                          <Spinner animation="border" size="sm" />
+                          Actualizando estado...
+                        </div>
+                      ) : (
+                        <>
+                          <Form.Label
+                            htmlFor={`status-${order.id}`}
+                            className="visually-hidden"
+                          >
+                            Cambiar estado de la orden {order.id.slice(0, 6)}
+                          </Form.Label>
+                          <Form.Select
+                            id={`status-${order.id}`}
+                            name={`status-${order.id}`}
+                            size="sm"
+                            value={order.status}
+                            disabled={updatingOrderId === order.id}
+                            onClick={(e) => e.stopPropagation()}
+                            onChange={(e) =>
+                              handleStatusChange(
+                                order.userId,
+                                order.id,
+                                e.target.value as Order["status"]
+                              )
+                            }
+                            style={{ width: "150px" }}
+                          >
+                            <option value="pending">Pendiente</option>
+                            <option value="processing">Procesando</option>
+                            <option value="completed">Completada</option>
+                            <option value="cancelled">Cancelada</option>
+                          </Form.Select>
+                        </>
+                      )}
+                    </div>
                   </div>
-                </div>
-              </Accordion.Header>
+                </Accordion.Header>
 
-              <Accordion.Body>
-                <h6 className="custom__text-muted mb-3">
-                  Productos ({order.items.length})
-                </h6>
+                <Accordion.Body>
+                  <h6 className="custom__text-muted mb-3">
+                    Productos ({order.items.length})
+                  </h6>
 
-                <OrderItemsList items={order.items} />
+                  <OrderItemsList items={order.items} />
 
-                <OrderTableSummary
-                  subtotal={order.subtotal}
-                  shipping={order.shipping}
-                />
-              </Accordion.Body>
-            </Card>
-          </Accordion.Item>
-        ))}
-      </Accordion>
+                  <OrderTableSummary
+                    subtotal={order.subtotal}
+                    shipping={order.shipping}
+                  />
+                </Accordion.Body>
+              </Card>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )}
 
       <ToastContainer
         position="bottom-left"
