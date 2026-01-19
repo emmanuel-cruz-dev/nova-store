d4a09208041f2c4550493ad8e86f3a863438d5de
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-19 07:55:08 -0300

    feat: updates routes constants in src/routes/config.tsx

--- src/routes/config.tsx
+++ src/routes/config.tsx
@@ -4,10 +4,13 @@ const Home = lazy(() => import("../pages/Home"));
 const NotFound = lazy(() => import("../pages/NotFound"));
 const Login = lazy(() => import("../pages/Login"));
 const Register = lazy(() => import("../pages/Register"));
-const Profile = lazy(() => import("../pages/Profile"));
-const Cart = lazy(() => import("../pages/Cart"));
 const Products = lazy(() => import("../pages/Products"));
 const Product = lazy(() => import("../pages/Product"));
+
+const Cart = lazy(() => import("../pages/Cart"));
+const Account = lazy(() => import("../pages/Account"));
+
+const Admin = lazy(() => import("../pages/Admin"));
 
 export const publicRoutes = [
   { path: "/", element: <Home /> },
@@ -25,9 +28,11 @@ export const publicRoutes = [
   },
 ];
 
-export const privateRoutes = [
-  { path: "/profile/:section?", element: <Profile /> },
+export const customerRoutes = [
   { path: "/cart", element: <Cart /> },
+  { path: "/account/:section?", element: <Account /> },
 ];
+
+export const adminRoutes = [{ path: "/admin/:section?", element: <Admin /> }];
 
 export const fallbackRoute = { path: "*", element: <NotFound /> };
