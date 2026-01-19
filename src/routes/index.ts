fe993466895ba70d386f400d5e03b08c92f4459c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-19 07:56:35 -0300

    feat: adds new routes exports to src/routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,10 @@
 export { default as AppRouter } from "./AppRouter";
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as PublicRoute } from "./PublicRoute";
-export { publicRoutes, privateRoutes, fallbackRoute } from "./config";
+export { default as AdminRoute } from "./AdminRoute";
+export {
+  publicRoutes,
+  customerRoutes,
+  adminRoutes,
+  fallbackRoute,
+} from "./config";
