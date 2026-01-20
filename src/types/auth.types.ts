f67b8f080104807007cb60abc852695b9e657a2c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-20 10:03:07 -0300

    feat: adds SafeUser type and updates AuthState in src/types/auth.types.ts

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,14 +1,16 @@
 import { User } from "./user.types";
 
+export type SafeUser = Omit<User, "password">;
+
 export interface AuthState {
   isAuthenticated: boolean;
-  user: User | null;
+  user: SafeUser | null;
   loading: boolean;
   authLoading: boolean;
-  register: (userData: RegisterData) => Promise<User>;
-  login: (email: string, password: string) => Promise<User>;
+  register: (userData: RegisterData) => Promise<SafeUser>;
+  login: (email: string, password: string) => Promise<SafeUser>;
   logout: () => void;
-  updateUserProfile: (updateUserData: User) => Promise<User>;
+  updateUserProfile: (updateUserData: SafeUser) => Promise<SafeUser>;
   initializeAuth: () => void;
 }
 
