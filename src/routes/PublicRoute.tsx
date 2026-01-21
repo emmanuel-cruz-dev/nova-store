cff57a8e2eed40a721c0571cb83f3e8456f53cdb
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-21 09:33:37 -0300

    feat: adds loading and authLoading to src/routes/PublicRoute.tsx

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -1,11 +1,20 @@
 import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../stores";
+import { Loader } from "../components";
 
 function PublicRoute({ children }: { children: ReactNode }) {
-  const { user } = useAuthStore();
+  const { user, loading, authLoading } = useAuthStore();
+
+  if (loading || authLoading) {
+    return <Loader />;
+  }
 
   if (user) {
+    if (user.role === "admin") {
+      return <Navigate to="/admin" replace />;
+    }
+
     return <Navigate to="/" replace />;
   }
 
