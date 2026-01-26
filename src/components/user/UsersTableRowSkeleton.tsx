2350f5e42974fef2a52fc5636335222764a50df1
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-26 08:53:23 -0300

    feat: modifies structure and styles in src/components/user/UsersTableRowSkeleton.tsx

--- src/components/user/UsersTableRowSkeleton.tsx
+++ src/components/user/UsersTableRowSkeleton.tsx
@@ -3,9 +3,32 @@ import { Placeholder } from "react-bootstrap";
 function UsersTableRowSkeleton() {
   return (
     <tr>
+      <td className="align-middle text-center">
+        <Placeholder animation="wave">
+          <Placeholder style={{ width: 16, height: 16 }} className="rounded" />
+        </Placeholder>
+      </td>
+      <td>
+        <div className="d-flex align-items-center gap-2">
+          <Placeholder animation="wave">
+            <Placeholder
+              style={{ width: 32, height: 32 }}
+              className="rounded-circle"
+            />
+          </Placeholder>
+          <Placeholder animation="wave" className="flex-grow-1">
+            <Placeholder xs={8} className="rounded" />
+          </Placeholder>
+        </div>
+      </td>
       <td>
         <Placeholder animation="wave">
-          <Placeholder xs={4} className="rounded" />
+          <Placeholder xs={10} className="rounded" />
+        </Placeholder>
+      </td>
+      <td>
+        <Placeholder animation="wave">
+          <Placeholder xs={3} className="rounded" />
         </Placeholder>
       </td>
       <td>
@@ -14,19 +37,20 @@ function UsersTableRowSkeleton() {
         </Placeholder>
       </td>
       <td>
-        <Placeholder animation="wave">
-          <Placeholder xs={8} className="rounded" />
-        </Placeholder>
-      </td>
-      <td>
-        <Placeholder animation="wave">
-          <Placeholder xs={12} className="rounded" />
-        </Placeholder>
-      </td>
-      <td>
-        <Placeholder animation="wave">
-          <Placeholder xs={6} className="rounded" />
-        </Placeholder>
+        <div className="d-flex gap-2">
+          <Placeholder animation="wave">
+            <Placeholder
+              style={{ width: 90, height: 24 }}
+              className="rounded"
+            />
+          </Placeholder>
+          <Placeholder animation="wave">
+            <Placeholder
+              style={{ width: 90, height: 24 }}
+              className="rounded"
+            />
+          </Placeholder>
+        </div>
       </td>
     </tr>
   );
