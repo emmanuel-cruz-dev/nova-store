15270b84b181f3b772098f646c7186321346f868
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-26 09:04:19 -0300

    feat: adds checkbox functions to src/components/user/UserTableHeader.tsx

--- src/components/user/UserTableHeader.tsx
+++ src/components/user/UserTableHeader.tsx
@@ -1,7 +1,35 @@
-function UserTableHeader() {
+import { Form } from "react-bootstrap";
+import { useEffect, useRef } from "react";
+import { CheckboxState } from "../../types";
+
+function UserTableHeader({
+  checkboxState,
+  onToggleSelectAll,
+}: {
+  checkboxState: CheckboxState;
+  onToggleSelectAll: () => void;
+}) {
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (checkboxRef.current) {
+      checkboxRef.current.indeterminate = checkboxState === "indeterminate";
+    }
+  }, [checkboxState]);
+
   return (
     <thead>
       <tr>
+        <th className="text-center" style={{ width: "50px" }}>
+          <Form.Check
+            id="select-all-users"
+            ref={checkboxRef}
+            type="checkbox"
+            checked={checkboxState === "checked"}
+            onChange={onToggleSelectAll}
+            aria-label="Seleccionar todos los usuarios"
+          />
+        </th>
         <th>Nombre Completo</th>
         <th>Email</th>
         <th>Órdenes</th>
