559871af7bc473540932e5f5c759981c6d4f0e9f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-26 09:09:59 -0300

    feat: adds checkbox functions to src/components/product/ProductTableHeader.tsx

--- src/components/product/ProductTableHeader.tsx
+++ src/components/product/ProductTableHeader.tsx
@@ -1,7 +1,35 @@
-function ProductTableHeader() {
+import { Form } from "react-bootstrap";
+import { useEffect, useRef } from "react";
+import { CheckboxState } from "../../types";
+
+function ProductTableHeader({
+  checkboxState,
+  onToggleSelectAll,
+}: {
+  checkboxState: CheckboxState;
+  onToggleSelectAll: () => void;
+}) {
+  const checkboxRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (checkboxRef.current) {
+      checkboxRef.current.indeterminate = checkboxState === "indeterminate";
+    }
+  }, [checkboxState]);
+
   return (
     <thead>
       <tr>
+        <th className="text-center" style={{ width: "50px" }}>
+          <Form.Check
+            id="select-all"
+            ref={checkboxRef}
+            type="checkbox"
+            checked={checkboxState === "checked"}
+            onChange={onToggleSelectAll}
+            aria-label="Seleccionar todos los productos"
+          />
+        </th>
         <th>Nombre</th>
         <th>Precio</th>
         <th>Categoria</th>
