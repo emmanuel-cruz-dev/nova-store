cc2295d9713f4538d742fb38d55f83a1fb65f18f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-27 10:24:16 -0300

    feat: adds hasAdminAccess to src/routes/PublicOrCustomerRoute.tsx

--- src/routes/PublicOrCustomerRoute.tsx
+++ src/routes/PublicOrCustomerRoute.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { useAuthStore } from "../stores";
 import { Loader } from "../components";
+import { hasAdminAccess } from "../utils";
 
 function PublicOrCustomerRoute({ children }: { children: ReactNode }) {
   const { user, loading, authLoading } = useAuthStore();
@@ -10,7 +11,7 @@ function PublicOrCustomerRoute({ children }: { children: ReactNode }) {
     return <Loader />;
   }
 
-  if (user?.role === "admin") {
+  if (hasAdminAccess(user?.role)) {
     return <Navigate to="/admin" replace />;
   }
 
