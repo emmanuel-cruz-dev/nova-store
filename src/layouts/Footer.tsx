677821d5f99410f6c2b3d6b3d7bd9da53d5e44e7
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-27 10:02:34 -0300

    feat: adds hasAdminAcces to src/layouts/Footer.tsx

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,10 +1,11 @@
 import { useAuthStore } from "../stores";
 import { AdminFooter, CustomerFooter } from "../components";
+import { hasAdminAccess } from "../utils";
 import "./Footer.css";
 
 function Footer() {
   const { user } = useAuthStore();
-  const isAdmin = user?.role === "admin";
+  const isAdmin = hasAdminAccess(user?.role);
 
   return (
     <footer className="footer bg-black text-white py-md-2" id="footer">
