59b7e541b162822cda6e31346fb2ac515ae417c9
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-01-28 10:22:22 -0300

    feat: adds index.ts to src/api/

---
+++ src/api/index.ts
@@ -0,0 +1,5 @@
+export * from "./config/axiosConfig";
+export * from "./services/user.service";
+export * from "./services/product.service";
+export * from "./services/order.service";
+export * from "./services/dashboard.service";
