dd4e323a3f2af52e33d396c2f82473c1f6972882
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-04 09:20:37 -0300

    feat: adds config.py to backend/app/core/

---
+++ backend/app/core/config.py
@@ -0,0 +1,33 @@
+from pydantic import BaseSettings
+from typing import List
+
+
+class Settings(BaseSettings):
+    # App
+    PROJECT_NAME: str = "Nova Store API"
+    VERSION: str = "1.0.0"
+    DEBUG: bool = False
+
+    # Database
+    DATABASE_URL: str
+
+    # JWT
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    # CORS
+    ALLOWED_ORIGINS: List[str] = [
+        "http://localhost:5173",
+        "https://nova-store-shop.vercel.app"
+    ]
+
+    model_config = {
+        "env_file": ".env",
+        "env_file_encoding": "utf-8",
+        "case_sensitive": True,
+        "extra": "ignore"
+    }
+
+
+settings = Settings()
