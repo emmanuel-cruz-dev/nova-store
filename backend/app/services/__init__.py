4cc15eaef0b422775ca613a4ed41e8de9d978edb
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-06 13:43:54 -0300

    feat: adds ProductService to backend/app/services/__init__.py

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,4 +1,5 @@
 from app.services.user_service import UserService
 from app.services.auth_service import AuthService
+from app.services.product_service import ProductService
 
-__all__ = ["UserService", "AuthService"]
+__all__ = ["UserService", "AuthService", "ProductService"]
