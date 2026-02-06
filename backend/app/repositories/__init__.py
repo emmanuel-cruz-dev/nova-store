727b51f3d16f49454929d0f4ba35e0ef1c102767
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2026-02-06 10:09:38 -0300

    feat: adds ProductRepository to backend/app/repositories/__init__.py

--- backend/app/repositories/__init__.py
+++ backend/app/repositories/__init__.py
@@ -1,3 +1,4 @@
 from app.repositories.user_repository import UserRepository
+from app.repositories.product_repository import ProductRepository
 
-__all__ = ["UserRepository"]
+__all__ = ["UserRepository", "ProductRepository"]
